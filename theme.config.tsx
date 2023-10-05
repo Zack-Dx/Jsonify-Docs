3a526bfb69f44a80207792cfa8c20864899f8186
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-10-06 00:26:18 +0530

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Jsonify API</span>,
+  project: {
+    link: '',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  footer: {
+    text: 'JSONIFY API',
+  },
+}
+
+export default config
